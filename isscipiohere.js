bfbbe5cac6e8499c40389e531bebfeda5e01f439
Stoodkev <quentin.correa@gmail.com>
2018-03-16 00:56:54 +0800

    Browser ready!

--- isscipiohere.js
+++ isscipiohere.js
@@ -1,29 +1,36 @@
 var steem = require('steem');
+const express=require('express');
+const app = express()
 
-steem.api.getState('@scipio/comments', function(err, result) {
-        var obj=JSON.parse(JSON.stringify(result.content));
-        var lastKill=null;
-        for(comment in obj)
-        {
-          if (obj[comment].body.match("cannot be approved")){
-          lastKill=obj[comment].created;
-          break;
+app.get('/', (req, res) => {
+  steem.api.getState('@scipio/comments', function(err, result) {
+          var obj=JSON.parse(JSON.stringify(result.content));
+          var lastKill=null;
+          for(comment in obj)
+          {
+            if (obj[comment].body.match("cannot be approved")){
+            lastKill=obj[comment].created;
+            break;
+            }
           }
-        }
-        if(lastKill==null)
-        {
-          console.log('Looks like he is sick, you are safe!');
-        }
-        else{
-        var timeSinceLastPown = new Date().getTime()-new Date(lastKill).getTime();
-        timeSinceLastPown= timeSinceLastPown/3600000 + new Date().getTimezoneOffset()/60;
-        if(timeSinceLastPown<1)
-          console.log('He is here and he is out for blood! Run!');
-        else if(timeSinceLastPown<2)
-          console.log('He moderated the shit out your fellows not so long ago, I wouldnt risk it!');
-        else if(timeSinceLastPown<5)
-          console.log('He is gone but he can come back!');
-        else if (timeSinceLastPown<22)
-          console.log('It s time for him to come back hunting!')
+          if(lastKill==null)
+          {
+            res.send('Looks like he is sick, you are safe!');
           }
-    });
+          else{
+          var timeSinceLastPown = new Date().getTime()-new Date(lastKill).getTime();
+          timeSinceLastPown= timeSinceLastPown/3600000 + new Date().getTimezoneOffset()/60;
+          if(timeSinceLastPown<1)
+            res.send('He is here and he is out for blood! Run!');
+          else if(timeSinceLastPown<2)
+            res.send('He moderated the shit out your fellows not so long ago, I wouldnt risk it!');
+          else if(timeSinceLastPown<5)
+            res.send('He is gone but he can come back!');
+          else if (timeSinceLastPown<22)
+            res.send('It s time for him to come back hunting!')
+            }
+      });
+
+})
+
+app.listen(process.env.PORT||3000, () => console.log('Scipio listening to you on port 3000!'))
